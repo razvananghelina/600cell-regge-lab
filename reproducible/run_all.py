dd7e02921e3173c13b06495bf3cd5489fb9d044d
razvananghelina <razvananghelina@roweb.ro>
2026-08-11 18:56:34 +0300

    Close minimal Whitney first-class conversion

--- reproducible/run_all.py
+++ reproducible/run_all.py
@@ -67,6 +67,7 @@ scripts = [
     "verify_whitney_neighbour_constraints.py",
     "verify_whitney_constraint_dirac_bergmann.py",
     "verify_whitney_positive_metric_completion.py",
+    "verify_whitney_first_class_conversion.py",
     "verify_tetrahedral_dirac_walk_bridge.py",
     "verify_tetrahedral_dirac_walk_robust.py",
     "verify_tetrahedral_dirac_walk_connectivity.py",
